b7a2ce6c6a7318c69e2b6d5ca0f829c40f85d406
Levreka <90356052+Levreka@users.noreply.github.com>
2021-11-01 22:29:10 -0700

    Add files via upload

---
+++ Vacation_Itinerary/config.py
@@ -0,0 +1,4 @@
+weather_api_key="95b7a2bbfe6591b746009b4c0aaefbd0"
+
+# Google API Key
+g_key="AIzaSyBdfU8xPX6_DHoAGsKT15DvZZnaERdgQNI"
---
+++ Weather_Database/config.py
@@ -0,0 +1,4 @@
+weather_api_key="95b7a2bbfe6591b746009b4c0aaefbd0"
+
+# Google API Key
+g_key="AIzaSyCMJvXH97OfD1fXjNrBvjimR-qcvYOAwzw"
